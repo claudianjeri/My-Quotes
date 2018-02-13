8a1069145b9bda023a1e53c380dcb253e632150c
claudia njeri <claudianjeri04@gmail.com>
2018-02-13 21:11:44 +0300

    added a constructor and instances in the service

--- src/app/quote-class/quote.ts
+++ src/app/quote-class/quote.ts
@@ -1,4 +1,8 @@
 export class Quote {
-    constructor( public quote: string, public author: string){}
-    
-}
+    public quote: string;
+    public author:string;
+
+    constructor(  quote: string,  author: string){
+    this.quote = quote;
+    this.author = author;
+}}
