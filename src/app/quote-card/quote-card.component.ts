455b2cd04882bf69e028e4bc2fef55216baa6390
claudia njeri <claudianjeri04@gmail.com>
2018-02-14 07:45:55 +0300

    did the imports and constructors in quote-card.comp.ts

--- src/app/quote-card/quote-card.component.ts
+++ src/app/quote-card/quote-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { QuoteService } from '../quote/quote.service';
+import {Quote} from '../quote-class/quote'
 
 @Component({
   selector: 'app-quote-card',
@@ -6,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-card.component.css']
 })
 export class QuoteCardComponent implements OnInit {
+  public quote = new Quote('', '');
+  constructor(public quoteService : QuoteService) {
+    this.getQuote();
+   }
 
-  constructor() { }
 
+  public getQuote(){
+    this.quoteService.getRandomQuote().subscribe((data: Quote) => {
+      this.quote = new Quote(data.quote, data.author);
+      console.log(this.quote);
+    });
+  }
   ngOnInit() {
   }
 
