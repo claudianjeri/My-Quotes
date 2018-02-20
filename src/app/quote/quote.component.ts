fe18f4bdd72c7200a98f45f191902a2a499e74d8
claudia njeri <claudianjeri04@gmail.com>
2018-02-20 12:47:06 +0300

    added ngFor directive

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // import { HttpClient } from '@angular/common/http';
-import { Quote } from '@angular/compiler';
+// import { Quote } from '@angular/compiler';
+import { Quote } from './quote'
 
 
 @Component({
